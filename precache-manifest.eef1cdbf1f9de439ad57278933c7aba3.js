cddda8826fc98e1c1b0fa0c697b15023a641faf1
Abolfazl <a.baghshahi1327@gmail.com>
2020-04-27 00:22:43 +0430

    Updates

---
+++ precache-manifest.eef1cdbf1f9de439ad57278933c7aba3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85a8ae7d00ce5d3731f6845a81c004ec",
+    "url": "/cloth-shop/index.html"
+  },
+  {
+    "revision": "684bd064e0f561653ae4",
+    "url": "/cloth-shop/static/css/main.fad74c09.chunk.css"
+  },
+  {
+    "revision": "90710e1d1dd2fdfff3f6",
+    "url": "/cloth-shop/static/js/2.c4da1fc3.chunk.js"
+  },
+  {
+    "revision": "7ba5a91c6aa4ae9cc068a25fb13d169c",
+    "url": "/cloth-shop/static/js/2.c4da1fc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "684bd064e0f561653ae4",
+    "url": "/cloth-shop/static/js/main.b5b702ef.chunk.js"
+  },
+  {
+    "revision": "5eaf1f7c74538284fcf9",
+    "url": "/cloth-shop/static/js/runtime-main.780fc0a1.js"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/cloth-shop/static/media/11.1 shopping-bag.svg.30f07071.svg"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/cloth-shop/static/media/4.3 crown.svg.ed28accb.svg"
+  }
+]);
